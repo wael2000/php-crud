334ea5a769d88d3e3aaaf212ce6ad27e29d03a10
wael <weldoami@redhat.com>
2019-07-05 16:04:05 +0400

    first commit

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
- 
+define('DB_SERVER', getenv("DATABASE_SERVICE_NAME"));
+define('DB_USERNAME', getenv("DATABASE_USER"));
+define('DB_PASSWORD', getenv("DATABASE_PASSWORD"));
+define('DB_NAME', getenv("MYSQL_DATABASE"));
+
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
