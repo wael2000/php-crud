529ef211673b667071ec9c09984a6b59c51c6bc3
wael <weldoami@redhat.com>
2019-07-05 16:44:50 +0400

    first commit

--- migrate-database.sh
+++ migrate-database.sh
@@ -1,1 +1,2 @@
-mysql -h 127.0.0.1 -u $MYSQL_USER -p$MYSQL_PASSWORD -D default -e "CREATE TABLE IF NOT EXISTS employees ( id int(11) NOT NULL AUTO_INCREMENT, name varchar(100) NOT NULL, address varchar(255) NOT NULL, salary int(10) NOT NULL, PRIMARY KEY (id) );";
+#mysql -h 127.0.0.1 -u $MYSQL_USER -p$MYSQL_PASSWORD -D default -e "CREATE TABLE IF NOT EXISTS employees ( id int(11) NOT NULL AUTO_INCREMENT, name varchar(100) NOT NULL, address varchar(255) NOT NULL, salary int(10) NOT NULL, PRIMARY KEY (id) );";
+mysql -h 127.0.0.1 -u $DATABASE_USER -p$DATABASE_PASSWORD -D default -e "CREATE TABLE IF NOT EXISTS employees ( id int(11) NOT NULL AUTO_INCREMENT, name varchar(100) NOT NULL, address varchar(255) NOT NULL, salary int(10) NOT NULL, PRIMARY KEY (id) );";
