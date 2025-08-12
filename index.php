4fdb207be795793c5529398773c2995492ae1671
Wael Eldoamiry <waelibrahim2000@hotmail.com>
2025-08-12 10:22:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
                 <div class="col-md-12">
                     <div class="page-header clearfix">
                         <h2 class="pull-left">Employees Details</h2>
+                        <h3 class="pull-left"> getenv("ENV") </h3> 
                         <a href="create.php" class="btn btn-success pull-right">Add New Employee</a>
                     </div>
                     <?php
